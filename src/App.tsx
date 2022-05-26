b42c9b970cdf552ca34376ad991604d3b2613acc
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-05-26 15:24:25 +0100

    finished with css

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className='calculator-grid'>
       <div className='output'>
-        <div className='previous-operand'>123,234 *</div>
-        <div className='current-operand'>987,654</div>
+        <div className='previous-operand'></div>
+        <div className='current-operand'></div>
       </div>
       <button className='span-two'>AC</button>
       <button>DEL</button>
